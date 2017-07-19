66346ff85a6b9e5d351f73250bbf8c06becfcd3a
Muhammed Thasneem P Android <muhammed.thasneem@yahoo.com>
2017-07-19 14:39:57 +0530

    Initial commit

---
+++ IOSNotification/IOSNotification/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IOSNotification
+//
+//  Created by Muhammed Thasneem on 19/07/17.
+//  Copyright © 2017 Muhammed Thasneem. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
